8ab40adbc203208579226e0ee74a71ae9a7abe71
bdeket <bedeke@hotmail.com>
2022-11-09 11:38:49 +0100

    apply-hook name

--- rktsicm/sicm/rkt/applyhook.rkt
+++ rktsicm/sicm/rkt/applyhook.rkt
@@ -3,4 +3,5 @@
 (provide (all-defined-out))
 (struct apply-hook (procedure [extra #:mutable])
   #:property prop:procedure (λ (this . args) (apply (apply-hook-procedure this) args))
-  #:extra-constructor-name make-apply-hook)
+  #:extra-constructor-name make-apply-hook
+  #:property prop:object-name (λ (ah) (object-name (apply-hook-procedure ah))))
