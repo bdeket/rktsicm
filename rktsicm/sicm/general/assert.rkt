d2374921b7cb6521062d3c0db3baa67daaef6527
bdeket <bedeke@hotmail.com>
2022-11-15 14:45:17 +0100

    assert -> don't quote proc

--- rktsicm/sicm/general/assert.rkt
+++ rktsicm/sicm/general/assert.rkt
@@ -7,4 +7,4 @@
   (syntax-case stx ()
     [(_ test) (quasisyntax/loc stx (assert test "assertion failed"))]
     [(_ test msg) (quasisyntax/loc stx (unless test #,(syntax/loc stx (error (format "~a:\n\tassertion failed: ~a"msg 'test)))))]
-    [(_ test msg proc ...) (quasisyntax/loc stx (unless test #,(syntax/loc stx (error (format "~a: ~a" '(proc ...) msg)))))]))
+    [(_ test msg proc ...) (quasisyntax/loc stx (unless test #,(syntax/loc stx (error (format "~a: ~a" (list proc ...) msg)))))]))
