0a7a25079a22218f27c4bac0e85e245c4cfe07c2
bdeket <bedeke@hotmail.com>
2022-11-21 19:17:03 +0100

    avoid clash between racket/time and mechanics/time

--- rktsicm/sicm/main.rkt
+++ rktsicm/sicm/main.rkt
@@ -1,8 +1,8 @@
 #lang s-exp "kernel.rkt"
 
 (require "calculus.rkt"
-         (except-in "kernel.rkt" raise)
-         (except-in "mechanics.rkt" Lie-derivative time)
+         (except-in "kernel.rkt" raise time)
+         (except-in "mechanics.rkt" Lie-derivative)
          "numerics.rkt"
          "parameters.rkt"
          "poly.rkt"
