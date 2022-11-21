0ac914288286ed10c45ab5cab0f2127cd4c1d401
bdeket <bedeke@hotmail.com>
2022-11-21 19:00:49 +0100

    helpers:
    * check-simplified timeout option
    * accumulate

--- rktsicm/sicm/tests/helper.rkt
+++ rktsicm/sicm/tests/helper.rkt
@@ -1,6 +1,9 @@
 #lang racket/base
 
 (require (for-syntax racket/base)
+         ;; ../main.rkt shadows raise, in modules this is not a problem, but if you would use
+         ;; check-simplified in the repl, I wan't to at least avoid one possible error...
+         (rename-in racket/base [raise dont-shadow-my-raise])
          rackunit
          racket/match
          racket/list
@@ -10,6 +13,7 @@
          "../display/suppress-args.rkt")
 
 (provide check-unique-match? check-simplified?
+         accumulate
          clear-arguments suppress-arguments rename-part)
 
 ;***************************************************************************************************
@@ -68,26 +72,58 @@
 ;***************************************************************************************************
 (define-syntax (check-simplified? stx)
   (syntax-case stx ()
-    [(_ term pattern)
+    [(_ term pattern #:timeout t)
      (with-syntax ([loc (list (syntax-source stx)
                               (syntax-line stx)
                               (syntax-column stx)
                               (syntax-position stx)
                               (syntax-span stx))])
-       #`(let ([T (simplify (rename-expression (arg-suppressor+ (simplify term))))]
-               [P (simplify (rename-expression (arg-suppressor+ (simplify pattern))))])
-           (with-check-info*
-            (list (make-check-name '|check-simplified?|)
-                  (make-check-actual T)
-                  (make-check-expected P)
-                  (make-check-location 'loc))
-            (λ () (check-equal? T P)))))]))
+       #`(let* ([cT (make-channel)]
+                [cP (make-channel)]
+                [T (thread (λ () (channel-put cT (with-handlers ([exn? (λ (e) e)])
+                                                   (simplify (rename-expression (arg-suppressor+ (simplify term))))))))]
+                [P (thread (λ () (channel-put cP (with-handlers ([exn? (λ (e) e)])
+                                                   (simplify (rename-expression (arg-suppressor+ (simplify pattern))))))))])
+           (define vT (sync/timeout t cT))(kill-thread T)
+           (when (exn? vT) (dont-shadow-my-raise vT))
+           (define vP (and vT (sync/timeout t cP)))(kill-thread P)
+           (when (exn? vP) (dont-shadow-my-raise vP))
+           (cond
+             [(eq? #f vT)
+              (with-check-info* (list (make-check-name '|check-simplified?|)
+                                      (make-check-params '(simplify term))
+                                      (make-check-message (format "timeout = ~as => simplification/calculation too difficult?" t))
+                                      (make-check-location 'loc))
+                                fail)]
+             [(eq? #f vP)
+              (with-check-info* (list (make-check-name '|check-simplified?|)
+                                      (make-check-params '(simplify pattern))
+                                      (make-check-message (format "timeout = ~as => simplification/calculation too difficult?" t))
+                                      (make-check-location 'loc))
+                                fail)]
+             [else
+              (with-check-info*
+               (list (make-check-name '|check-simplified?|)
+                     (make-check-actual vT)
+                     (make-check-expected vP)
+                     (make-check-location 'loc))
+               (λ () (check-equal? vT vP)))])))]
+    [(_ term pattern)
+     (syntax/loc stx (check-simplified? term pattern #:timeout #f))]))
 
 (module+ test
   (require "../main.rkt") ;; needed to use simplified
   (check-simplified? '(+ (* (sin x) (cos y)) (+ 4 5))
-                    '(+ 9 (* (cos y) (sin x))))
+                     '(+ 9 (* (cos y) (sin x))))
   (check-exn exn:fail?
              (λ ()
                (check-simplified? '(+ 9 (* (sin x) (cos y)))
-                                  '(+ 9 (* (sin y) (cos z)))))))
+                                  '(+ 9 (* (sin y) (cos z))))))
+  (check-exn exn:fail?
+             (λ () (check-simplified? (let ([a 1])(sleep 100)a) 1 #:timeout .1))))
+;***************************************************************************************************
+(define-syntax-rule (accumulate acc expr)
+  (let* ([val '()]
+         [acc (λ (v) (set! val (append val (list v))))])
+    expr
+    val))
