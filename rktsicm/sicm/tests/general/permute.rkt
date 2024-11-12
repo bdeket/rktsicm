4cbbb571f0db5f37c54bf40228e5fa982e86da50
bdeket <bedeke@hotmail.com>
2024-11-12 11:57:15 +0100

    general/permute
    - extra tests

--- rktsicm/sicm/tests/general/permute.rkt
+++ rktsicm/sicm/tests/general/permute.rkt
@@ -31,7 +31,61 @@
               (check-equal? (factorial 12) 479001600))
    (test-case "number-of-combinations"
               (check-equal? (number-of-combinations 1000000 12)
-                            2087537916209397485013453738892186349699824088113864639583250000))))
+                            2087537916209397485013453738892186349699824088113864639583250000))
+   ;*************
+   (test-case "permutations"
+              (check-equal? (permutations '()) '(()))
+              (check-equal? (permutations '(a)) '((a)))
+              (check-equal? (permutations '(a b)) '((a b)(b a)))
+              (check-equal? (permutations '(a b c))
+                            '((a b c) (a c b) (b a c) (b c a) (c a b) (c b a))))
+   (test-case "combinations"
+              (check-equal? (combinations '() 1) '())
+              (check-equal? (combinations '(a) 1) '((a)))
+              (check-equal? (combinations '(a b) 1) '((a)(b)))
+              (check-equal? (combinations '(a b) 2) '((a b)))
+              (check-equal? (combinations '(a b c) 2) '((a b)(a c)(b c))))
+   (test-case "list-interchanges"
+              (check-equal? (list-interchanges '(1 2) '(1 2)) 0)
+              (check-equal? (list-interchanges '(1 2) '(2 1)) 1)
+              (check-equal? (list-interchanges '(1 2 3) '(3 2 1)) 3))
+   (test-case "split-permutations"
+              (check-equal? (split-permutations '(0 1) (permutations '(0 1)) list)
+                            '(((0 1)) ((1 0))))
+              (check-equal? (split-permutations '(a b c) (permutations '(a b c)) list)
+                            '(((c a b) (b c a) (a b c)) ((c b a) (b a c) (a c b)))))
+   (test-case "pertutation-interchanges"
+              (check-equal? (permutation-interchanges '(0 1 2)) 0)
+              (check-equal? (permutation-interchanges '(0 2 1.5)) 1)
+              (check-equal? (permutation-interchanges '(2 0 1.5)) 2))
+   (test-case "permute"
+              (check-equal? (permute '(2 1 0) '(c b a)) '(a b c)))
+   (test-case "sort-and-permute"
+              (let-values ([(org srt p-fct o-fct) (sort-and-permute '(5 2 8 9) < values)])
+                (check-equal? org '(5 2 8 9))
+                (check-equal? srt '(2 5 8 9))
+                (check-equal? (p-fct org) srt)
+                (check-equal? (o-fct srt) org)
+                (check-equal? (p-fct '(b a c d)) '(a b c d))))
+   (test-case "factorial"
+              (check-equal? (factorial 3) 6))
+   (test-case "number-of-permutations"
+              (check-equal? (number-of-permutations 8) (factorial 8)))
+   (test-case "exact-quotient"
+              (check-equal? (exact-quotient 8 2) 4)
+              (check-exn #px"^assertion failed: \\(= 0 r\\)$" (λ () (exact-quotient 8 3))))
+   (test-case "binomial-coefficient"
+              (check-equal? (binomial-coefficient 0 0) 1)
+              (check-equal? (binomial-coefficient 10 0) 1)
+              (check-equal? (binomial-coefficient 10 3) 120))
+   (test-case "number-of-combinations"
+              (check-equal? (number-of-combinations 10 3) (binomial-coefficient 10 3))
+              (check-equal? (number-of-combinations 10 4) (binomial-coefficient 10 4)))
+   (test-case "permutation-parity"
+              (check-equal? (permutation-parity '(c b a) '(a b c)) -1)
+              (check-equal? (permutation-parity '(b c a) '(a b c))  1)
+              (check-equal? (permutation-parity '(c b a) '(3 2 1))  0))
+   ))
 
 (module+ test
   (require rackunit/text-ui)
