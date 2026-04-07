2d977c47b103ae649bbaeb2124e13db7d52c5ce7
bdeket <bedeke@hotmail.com>
2026-04-07 17:51:50 +0200

    tests
    poly/sparse-interpolate test that compared old vs new implementation was
    often failing. Change the test to be more permissive.

--- rktsicm/sicm/tests/simplify/sparse-interpolate.rkt
+++ rktsicm/sicm/tests/simplify/sparse-interpolate.rkt
@@ -57,28 +57,43 @@
                                              (vector->list coefficients))))))
                     (lambda (ignore) (fail))))))
     ;;; Check that the new algorithm is equivalent to the old one, and faster
+    (define timings (make-hash))
     (define (check m)
       (let ((xs (generate-list m interpolate-random))
             (fs (generate-list m interpolate-random)))
         (let ((t0 (current-milliseconds)))
           (univariate-interpolate-values xs fs
+                                         ;; succeed
                                          (lambda (new-result)
                                            (let ((t1 (current-milliseconds)))
                                              (old-univariate-interpolate-values xs fs
                                                                                 (lambda (old-result)
                                                                                   (let ((t2 (current-milliseconds)) (e (equal? old-result new-result)))
-                                                                                    (assert (<= (- t1 t0) (- t2 t1)))
-                                                                                    (assert e)))
+                                                                                    (assert e)
+                                                                                    (hash-update! timings '++ (λ (x) (list (+ (car x) 1)
+                                                                                                                           (+ (cadr x) (/ (- t1 t0) (- t2 t1) 1.))
+                                                                                                                           (max (caddr x) (/ (- t1 t0) (- t2 t1) 1.))))
+                                                                                                  (list 0 0 0))
+                                                                                    #;(assert (<= (- t1 t0) (- t2 t1))
+                                                                                            (format "1 (~a ~a) -- " (/ (- t1 t0) (- t2 t1) 1.) m))))
                                                                                 (lambda ()
-                                                                                  (pp (list 'old-failed-new-won xs fs new-result))))))
+                                                                                  (hash-update! timings '+- add1 0)
+                                                                                  'old-failed-new-won))))
+                                         ;; fail
                                          (lambda ()
                                            (let ((t1 (current-milliseconds)))
                                              (old-univariate-interpolate-values xs fs
                                                                                 (lambda (old-result)
+                                                                                  (hash-update! timings '-+ add1 0)
                                                                                   (pp (list 'new-failed-old-won xs fs old-result)))
                                                                                 (lambda ()
                                                                                   (let ((t2 (current-milliseconds)))
-                                                                                    (assert (<= (- t1 t0) (- t2 t1)))
+                                                                                    (hash-update! timings '-- (λ (x) (list (+ (car x) 1)
+                                                                                                                           (+ (cadr x) (/ (- t1 t0) (- t2 t1) 1.))
+                                                                                                                           (max (caddr x) (/ (- t1 t0) (- t2 t1) 1.))))
+                                                                                                  (list 0 0 0))
+                                                                                    #;(assert (<= (- t1 t0) (- t2 t1))
+                                                                                            (format "2 (~a ~a) -- " (/ (- t1 t0) (- t2 t1) 1.) m))
                                                                                     (void))
                                                                                   'both-failed))))))))
     (check-equal? (let lp ((i 10))
@@ -86,7 +101,28 @@
                         'done
                         (begin (check (+ 15 (random 50)))
                                (lp (fix:- i 1)))))
-                  'done))
+                  'done)
+    (println timings)
+    ;; Old never better
+    (check-false (hash-ref timings '-+ #f))
+    ;; when ok (~93% of cases): new never slower
+    (check-true (let ([X (hash-ref timings '++ '(0 0 0))])
+                  (or (<= (caddr X) 1.)
+                      ;; almost never
+                      (and (<= (/ (cadr X) (car X)) 0.9) (<= (caddr X) 3.5)))))
+    ;; when fail (~7% of cases): new generally not slower
+    (check-true (let ([X (hash-ref timings '++ '(0 0 0))])
+                  (or ;; almost never
+                      (<= (caddr X) 1.)
+                      ;; usually ~ 3
+                      (and (<= (caddr X) 5.0)
+                           (case (car X)
+                             [(1) #t]
+                             [(2) (<= (/ (cadr X) (car X)) 3.)]
+                             [(3 4) (<= (/ (cadr X) (car X)) 2.5)]
+                             [(5 6 7 8 9 10) (<= (/ (cadr X) (car X)) 2.)]
+                             [else (<= (/ (cadr X) (car X)) 1.75)])))))
+    )
    ))
 
 (module+ test
